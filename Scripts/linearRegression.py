8464e947d0f79e772681767fa55f9c9e6baf7010
Qiuhong <qiuhong@users.noreply.github.com>
2019-04-06 16:32:35 +0800

    linear regression

--- Scripts/linearRegression.py
+++ Scripts/linearRegression.py
@@ -33,12 +33,11 @@ A = getA(training)
 c = getc(rBar, trStats["ratings"])
 
 # compute the estimator b
+
 def param(A, c):
-    I = np.identity(trStats["n_movies"] + trStats["n_users"],dtype=None)
     AT = np.transpose(A)
     ATA = np.dot(AT,A)
-    lI = 0.000000000001*I
-    inverse = np.linalg.inv(ATA+lI) 
+    inverse = np.linalg.pinv(ATA) 
     ATc = np.matmul(AT,c)
     b = np.matmul(inverse,ATc)
     return b
@@ -46,12 +45,13 @@ def param(A, c):
 # compute the estimator b with a regularisation parameter l
 # note: lambda is a Python keyword to define inline functions
 #       so avoid using it as a variable name!
+
 def param_reg(A, c, l):
     I = np.identity(trStats["n_movies"] + trStats["n_users"],dtype=None)
     AT = np.transpose(A)
     ATA = np.matmul(AT,A)
     lI = l*I
-    inverse = np.linalg.inv(ATA+lI)    
+    inverse = np.linalg.pinv(ATA+lI)    
     ATc = np.matmul(AT,c)
     b = np.matmul(inverse,ATc)
     return b   
@@ -69,6 +69,54 @@ def predict(movies, users, rBar, b):
         p[i] = rating
     return p
 
+###########################################################################
+# Tuning of lambda
+    
+min_rmse=10
+min_regularization = 0
+rmselist= []
+#rrange = [0.001, 0.01, 0.1, 0.5, 1, 10]
+rrange = np.linspace(2.52, 2.55, num=30)
+for regularization in rrange:
+    b = param_reg(A, c, regularization)
+    rmse = lib.rmse(predict(vlStats["movies"], vlStats["users"], rBar, b), vlStats["ratings"])
+    rmselist.append(rmse)
+    if rmse < min_rmse:
+        min_rmse = rmse
+        min_regularization = regularization
+
+plt.plot(rrange, rmselist,'ro')
+
+plt.title("RMSE vs regularisation parameter")
+print ("Minimum: %f,%f" % (min_regularization, min_rmse))
+plt.show()
+
+
+
+lam = np.arange(0,10,0.2)
+def lam_param_reg(A,c,lam):
+    final = []
+    for i in lam:
+        bb = param_reg(A, c, i)
+        final.append(bb)
+    return final
+        
+abmc = []
+bb = []
+final = lam_param_reg(A,c,lam)
+for i in final: 
+    AB = np.matmul(A,i)
+    subtract = AB - c
+    square = np.matmul(np.transpose(subtract),subtract)
+    abmc.append(square)
+    bsquare = np.matmul(np.transpose(i),i)
+    bb.append(bsquare)
+    
+plt.plot(lam,abmc, label="||Ab-c||^T||Ab-c||")
+plt.plot(lam,bb,label="||b||^T||b||")
+plt.xlabel("Regularisation Parameter")
+plt.legend()
+
 # Unregularised version (<=> regularised version with l = 0)
 #l =0
 #b = param(A, c)
@@ -83,7 +131,8 @@ print ("RMSE for validation %f" % lib.rmse(predict(vlStats["movies"], vlStats["u
 
 ##Qns 1
 predictedRatings = np.array([predict(trStats["movies"], trStats["users"], rBar, b)])
-################################################################################################
+
+
 ##Output for 300 by 97
 def output(training,b):
     G = np.zeros((trStats["n_users"], trStats["n_movies"]))
@@ -94,38 +143,7 @@ def output(training,b):
             if rating < 1: rating = 1.0
             G[user][movie] = rating
     return G
-
-
-
-
-min_rmse=10
-min_regularization = 0
-rmselist= []
-#rrange = [0.001, 0.01, 0.1, 0.5, 1, 10]
-rrange = np.linspace(2.52, 2.55, num=30)
-for regularization in rrange:
-    b = param_reg(A, c, regularization)
-    #print "Linear regression, l = %f" % regularization
-    rmse = lib.rmse(predict(vlStats["movies"], vlStats["users"], rBar, b), vlStats["ratings"])
-    rmselist.append(rmse)
-    #print rmse
-    if rmse < min_rmse:
-        min_rmse = rmse
-        min_regularization = regularization
-
-plt.plot(rrange, rmselist,'ro')
-
-#plt.axis([6, 7, 1.069, 1.070])
-
-plt.title("RMSE vs regularisation parameter")
-print ("Minimum: %f,%f" % (min_regularization, min_rmse))
-plt.show()
-
-
-
 output = output(training,b)
-
-
-np.savetxt("Linearregression+v1.csv", output)
+#np.savetxt("Linearregression+v1.csv", output)
 
 
